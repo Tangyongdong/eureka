a53e630b11f129769483b49c017506e6043f55bc
tangyongdong <Tyd123zxz>
2018-05-11 10:07:58 +0800

    重构1.0.1

--- src/main/java/com/tangyongdong/eureka/EurekaApplication.java
+++ src/main/java/com/tangyongdong/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.tangyongdong.play.eureka;
+package com.tangyongdong.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
